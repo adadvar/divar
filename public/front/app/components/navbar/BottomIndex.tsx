f4c21e826a34045bc31c700ad02969623742f637
adadvar <a.dadvar_71@yahoo.com>
2023-09-27 17:31:38 +0330

    add ok nk to cityselectmobOverlay

--- public/front/app/components/navbar/BottomIndex.tsx
+++ public/front/app/components/navbar/BottomIndex.tsx
@@ -4,7 +4,7 @@ import RegisterAdvertButtonMob from "./RegisterAdvertButtonMob";
 import ChatButtonMob from "./ChatButtonMob";
 import ProfileButtonMob from "./ProfileButtonMob";
 
-const ButtomIndex = () => {
+const BottomIndex = () => {
     return (
         <div className="lg:hidden shadow-[0_5px_11px_rgba(0,0,0,0.35)] w-full fixed bottom-0">
             <div className="lg:hidden navbar justify-between items-center 2xl:container 2xl:px-16 mx-auto px-5">
@@ -21,4 +21,4 @@ const ButtomIndex = () => {
     );
 };
 
-export default ButtomIndex;
+export default BottomIndex;
--- public/front/app/components/navbar/city/CitySelectMobOverlay.tsx
+++ public/front/app/components/navbar/city/CitySelectMobOverlay.tsx
@@ -1,10 +1,13 @@
 import CitySearchBox from "./CitySearchBox";
 import CityItems from "./CityItems";
+import { useDispatch } from "react-redux";
+import { closeDialog } from "@/app/GlobalRedux/features/global/globalSlice";
 
 const CitySelectMobOverlay = () => {
+    const dispatch = useDispatch();
     return (
-        <div className="lg:hidden absolute top-0 left-0 right-0 bottom-0  bg-white m-auto rounded-md w-full z-50">
-            <div className="flex flex-col gap-5 py-8 overflow-hidden h-full">
+        <div className="lg:hidden absolute top-0 left-0 right-0 bottom-0  bg-white m-auto rounded-md w-full h-screen z-50">
+            <div className="flex flex-col space-y-3 pt-4 overflow-hidden h-full">
                 <div className="flex flex-col gap-5 px-10">
                     <div className="flex justify-between">
                         <h1 className="text-gray-900 font-bold">انتخاب شهر</h1>
@@ -16,6 +19,17 @@ const CitySelectMobOverlay = () => {
                 </div>
                 <div className="h-1 left-0 right-0 shadow-sm"></div>
                 <CityItems />
+                <div className="navbar w-full flex justify-around shadow-[rgba(0,0,0,0.1)_0px_-2px_5px]">
+                    <button
+                        className="btn btn-ghost border text-gray-500 border-gray-600 w-[45%]"
+                        onClick={() => dispatch(closeDialog())}
+                    >
+                        انصراف
+                    </button>
+                    <button className="btn btn-ghost bg-gray-200 text-gray-400 w-[45%]">
+                        تایید
+                    </button>
+                </div>
             </div>
         </div>
     );
