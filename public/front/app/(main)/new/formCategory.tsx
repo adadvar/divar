1764b8cae59c3e090455c3137e12739ad1a94ab8
adadvar <a.dadvar_71@yahoo.com>
2024-01-29 21:14:50 +0330

    add city and child to /new formCategory

--- public/front/app/(main)/new/formCategory.tsx
+++ public/front/app/(main)/new/formCategory.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { createAnswer } from "@/app/lib/actions";
+import { useGlobal, useTmp } from "@/app/store/global-store";
 import {
     FormElementInstance,
     FormElements,
@@ -18,6 +19,8 @@ const FormCategory = ({
     const formValues = useRef<{ [key: string]: string }>({});
     const formErrors = useRef<{ [key: string]: boolean }>({});
     const [renderKey, setRenderKey] = useState(new Date().getTime());
+    const { cities } = useGlobal();
+    const [selectedCity, setSelectedCity] = useState<number | undefined>();
 
     const submitValue = useCallback((key: string, value: string) => {
         formValues.current[key] = value;
@@ -40,7 +43,7 @@ const FormCategory = ({
         return true;
     }, [content]);
 
-    const submitForm = async () => {
+    const submitForm = async (e: FormData) => {
         formErrors.current = {};
         const validForm = validateForm();
         if (!validForm) {
@@ -63,30 +66,62 @@ const FormCategory = ({
         <div className="" key={renderKey}>
             <p className="">ثبت آگهی</p>
             <p className="">عکس آگهی</p>
-            {content &&
-                content.map((el) => {
-                    const FormElement = FormElements[el.type].formComponent;
-                    return (
-                        <FormElement
-                            key={el.id}
-                            elementInstance={el}
-                            submitValue={submitValue}
-                            isInvalid={formErrors.current[el.id]}
-                            defaultValue={formValues.current[el.id]}
-                        />
-                    );
-                })}
-            <div className="flex justify-end gap-x-2 my-5">
-                <button className="btn btn-ghost btn-hover border border-gray-500 text-gray-500 hover:text-gray-600">
-                    انصراف
-                </button>
-                <button
-                    className="btn btn-ghost btn-hover text-white bg-red-700 hover:bg-red-600"
-                    onClick={() => submitForm()}
+            <form action={submitForm} className="flex flex-col w-full my-8">
+                <label
+                    htmlFor="city_id"
+                    className="text-lg font-bold text-black mb-4"
                 >
-                    ارسال آگهی
-                </button>
-            </div>
+                    استان
+                </label>
+                <select
+                    name="city_id"
+                    className="bg-transparent input text-black border-gray-400 focus:border-red-800 mb-4"
+                    onChange={(e) => setSelectedCity(Number(e.target.value))}
+                >
+                    <option value=""></option>
+                    {cities.map((city) => (
+                        <option value={city.id}>{city.name}</option>
+                    ))}
+                </select>
+
+                <label
+                    htmlFor="city_id"
+                    className="text-lg font-bold text-black mb-4"
+                >
+                    شهر
+                </label>
+                <select
+                    name="city_id"
+                    className="bg-transparent input text-black border-gray-400 focus:border-red-800 mb-4"
+                >
+                    {selectedCity &&
+                        cities[selectedCity].child.map((city) => (
+                            <option value={city.id}>{city.name}</option>
+                        ))}
+                </select>
+
+                {content &&
+                    content.map((el) => {
+                        const FormElement = FormElements[el.type].formComponent;
+                        return (
+                            <FormElement
+                                key={el.id}
+                                elementInstance={el}
+                                submitValue={submitValue}
+                                isInvalid={formErrors.current[el.id]}
+                                defaultValue={formValues.current[el.id]}
+                            />
+                        );
+                    })}
+                <div className="flex justify-end gap-x-2 my-5">
+                    <button className="btn btn-ghost btn-hover border border-gray-500 text-gray-500 hover:text-gray-600">
+                        انصراف
+                    </button>
+                    <button className="btn btn-ghost btn-hover text-white bg-red-700 hover:bg-red-600">
+                        ارسال آگهی
+                    </button>
+                </div>
+            </form>
         </div>
     );
 };
