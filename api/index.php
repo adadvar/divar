dbeea31a494c34c59a67e776464b3533d6630d5d
adadvar <a.dadvar_71@yahoo.com>
2024-02-10 10:00:08 +0330

    add email service liara

---
+++ api/index.php
@@ -0,0 +1,9 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$response = $kernel->handle($request = Illuminate\Http\Request::capture());
+$reponse->send();
+$kernel->terminate($request, $response);
